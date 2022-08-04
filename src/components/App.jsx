07aabe7fee04211fbe31d9c2ed3365b775f0d25b
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-08-05 01:01:53 +0200

    add FriendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import FriendList from './FriendList';
+import friends from '../data/friends.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FriendList friends={friends} />
     </div>
   );
 };
+export default App;
